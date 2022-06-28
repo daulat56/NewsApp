d877d0b7f0699af7f734d3e725004708ed19d34b
daulat <20bcs037@iiitdwd.ac.in>
2022-06-28 13:38:07 +0530

    newsapp_Don

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        hello its me
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/components/NewsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class newsItems extends Component {
+  render() {
+    return (
+      <div>
+        sorry
+      </div>
+    )
+  }
+}
+
+export default newsItems
